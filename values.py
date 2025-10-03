69e0f3cfeb23c4e5c5f4724953cdb0e8cced1f54
mohidkittur3-tech <mohidkittur3@gmail.com>
2025-10-03 10:11:03 +0530

    hhh

---
+++ values.py
@@ -0,0 +1,9 @@
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print(f"Before swapping: a = {a}, b = {b}")
+    a, b = b, a
+    print(f"After swapping: a = {a}, b = {b}")
+except ValueError:
+    print("Please enter valid integers.")
+
